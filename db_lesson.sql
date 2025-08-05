6a302a9b4233d5b9adbbd79b90325540ddf66d8e
糸永　翔 <itonagasho@itonagashounoMacBook-Air.local>
2025-08-05 15:56:38 +0900

    db lesson Section10の回答修正（1）

--- db_lesson.sql
+++ db_lesson.sql
@@ -1,5 +1,5 @@
-Q1
-A, CREATE TABLE departments (
+--Q1
+CREATE TABLE departments (
   department_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -7,12 +7,12 @@ A, CREATE TABLE departments (
 );
 
 
-Q2
-A, ALTER TABLE people ADD department_id INT UNSIGNED AFTER email;
+--Q2
+ ALTER TABLE people ADD department_id INT UNSIGNED AFTER email;
 
 
-Q3
-A, INSERT INTO departments (name)
+--Q3
+INSERT INTO departments (name)
 VALUES
 ('営業'),
 ('開発'),
@@ -47,19 +47,19 @@ VALUES
 ('16', '来月のプロジェクトスケジュールを見直し、工数の再計算を行いました。');
 
 
-Q4
-A, UPDATE people SET department_id = 1 WHERE person_id = 1;
+--Q4
+UPDATE people SET department_id = 1 WHERE person_id = 1;
 UPDATE people SET department_id = 2 WHERE person_id = 2;
 UPDATE people SET department_id = 3 WHERE person_id = 3;
 UPDATE people SET department_id = 4 WHERE person_id = 4;
 UPDATE people SET department_id = 5 WHERE person_id = 6;
 
 
-Q5 年齢の降順で男性の名前と年齢を取得してください。
-A, SELECT name, age FROM people WHERE gender = 1 ORDER BY age DESC;
+--Q5 年齢の降順で男性の名前と年齢を取得してください。
+SELECT name, age FROM people WHERE gender = 1 ORDER BY age DESC;
 
 
-Q6 テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明してください。
+--Q6 テーブル・レコード・カラムという3つの単語を適切に使用して、下記のSQL文を日本語で説明してください。
 SELECT
   `name`, `email`, `age`
 FROM
@@ -69,23 +69,23 @@ WHERE
 ORDER BY
   `created_at`;
 
-A, peopleテーブルから、カラムであるdepartment_idが1に値する、name、email、ageの
-３つのレコードを、作成された日時の昇順(古い順)で取得する。
+/* A. peopleテーブルから、カラムであるdepartment_idが1に値する、name、email、ageの
+３つのレコードを、作成された日時の昇順(古い順)で取得する。*/
 
 
-Q7 20代の女性と40代の男性の名前一覧を取得してください。
-A, SELECT name FROM people WHERE age BETWEEN 20 AND 29 AND gender = 2 OR age BETWEEN 40 AND 49 AND gender = 1;
+--Q7 20代の女性と40代の男性の名前一覧を取得してください。
+SELECT name FROM people WHERE age BETWEEN 20 AND 29 AND gender = 2 OR age BETWEEN 40 AND 49 AND gender = 1;
 
 
-Q8 営業部に所属する人だけを年齢の昇順で取得してください。
-A, SELECT * FROM people WHERE department_id = 1 ORDER BY age;
+--Q8 営業部に所属する人だけを年齢の昇順で取得してください。
+SELECT * FROM people WHERE department_id = 1 ORDER BY age;
 
 
-Q9 開発部に所属している女性の平均年齢を取得してください。
-A, SELECT AVG(age) AS average_age FROM people WHERE department_id = 2 AND gender = 2 GROUP BY gender;
+--Q9 開発部に所属している女性の平均年齢を取得してください。
+SELECT AVG(age) AS average_age FROM people WHERE department_id = 2 AND gender = 2 GROUP BY gender;
 
-Q10  名前と部署名とその人が提出した日報の内容を同時に取得してください。（日報を提出していない人は含めない）
-A, SELECT p.name, d.name, r.content 
+--Q10  名前と部署名とその人が提出した日報の内容を同時に取得してください。（日報を提出していない人は含めない）
+SELECT p.name, d.name, r.content 
 FROM people p
 JOIN departments d 
 USING (department_id )
@@ -93,8 +93,8 @@ JOIN reports r
 USING (person_id);
 
 
-Q11  日報を一つも提出していない人の名前一覧を取得してください。
-A, SELECT p.name 
+--Q11  日報を一つも提出していない人の名前一覧を取得してください。
+SELECT p.name 
 FROM people p 
 LEFT OUTER JOIN reports r 
 USING (person_id)
